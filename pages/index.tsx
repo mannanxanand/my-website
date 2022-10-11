3c95c9a2cdbe838bec1590a48364b80ccc4cb7d1
Mannan Anand <mannananand@mannans-mbp.3520.dhcp.asu.edu>
2022-10-10 23:03:17 -0700

    escape quotes

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,8 +2,8 @@ import type { NextPage } from 'next'
 const Home: NextPage = () => {
   return (
     <div>        
-      <h3>Hi, I'm Mannan!</h3>
-      <p>I'm an incoming freshman pursuing B.S in Computer Science at Arizona State University. I'm a computer enthusiast and I love learning new 
+      <h3>Hi, I{"'"}m Mannan!</h3>
+      <p>I{"'"}m an incoming freshman pursuing B.S in Computer Science at Arizona State University. I{"'"}m a computer enthusiast and I love learning new 
 technologies. Find more about me about on my <a target="_blank" rel="noreferrer" href="https://linkedin.com/in/anandmannan">Linkedin Profile</a>.</p>
     </div>
   )
